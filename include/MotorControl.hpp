efd9d9183e14d0efca1ac5c755165bfc0117b1d2
JHSylte <jon.havard@hotmail.com>
2024-02-02 10:39:37 +0100

    created motorcontrol branch

---
+++ include/MotorControl.hpp
@@ -0,0 +1,12 @@
+#ifndef LINEFOLLOWINGROBOT2_MOTORCONTROL_HPP
+#define LINEFOLLOWINGROBOT2_MOTORCONTROL_HPP
+
+class MotorControl {
+public:
+
+private:
+    int maxSpeed;
+    int minSpeed;
+};
+
+#endif //LINEFOLLOWINGROBOT2_MOTORCONTROL_HPP
