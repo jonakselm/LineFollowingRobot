eb0f71448883c668b57f0b46b4a66ef677fe621b
Jon Aksel <jonamoll@stud.ntnu.no>
2024-02-05 14:34:15 +0000

    Sensor and pid has synergy

--- src/main.cpp
+++ src/main.cpp
@@ -9,31 +9,35 @@ Sensor sensor(sensorPins, 5);
 
 // Pid setup
 double kp = 1;
-double ki = 0.1;
+double ki = 0.0;
 double kd = 0.01;
 double setpoint = 2000;
 PIDController pid(kp, ki, kd,  setpoint);
+
+uint64_t elapsedTime = 0;
 
 void setup()
 {
     // Sensor setup
     Serial.begin(9600);
-    /*sensor.calibrate(600);
-    sensor.saveCalibration();*/
-    sensor.loadCalibration();
+    sensor.calibrate(75);
 }
 
 void loop()
 {
-    Serial.println(sensor.readLine());
+    uint16_t pos = sensor.readLine();
+    Serial.print("Pos: ");
+    Serial.println(pos);
+    // 0 == høyre, 4000 == venstre
+    //Serial.println(pos);
 
-    double current_value = 1000;
 
-
-    double dt = 1;
+    double dt = double(millis() - elapsedTime) / 1000;
+    elapsedTime = millis();
 
     // Compute PID output
-    double pid_output = pid.compute(current_value, dt);
+    double pid_output = pid.compute(pos, dt);
+    Serial.print("PID: ");
     Serial.println(pid_output);
 
     // Assuming you have code to apply the PID output to your system (e.g., a motor)
@@ -41,5 +45,6 @@ void loop()
 
     // Update setpoint if needed
     // pid.setSetpoint(new_desired_setpoint);
+    delay(500);
 }
 
